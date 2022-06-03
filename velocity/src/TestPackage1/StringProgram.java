185c4b1f58ebacdabfc5e1ba90082ded25587f9b
DELL <DELL@DESKTOP-MGVC6GB>
2022-06-03 21:56:42 +0530

    JavaProgram1

---
+++ velocity/src/TestPackage1/StringProgram.java
@@ -0,0 +1,28 @@
+package TestPackage1;
+
+public class StringProgram {
+
+	public static void main(String[] args) 
+	{
+		//String Literal
+		String a = "Vishal";
+		System.out.println(a);
+		
+		//String with a new Keyword
+		String c = new String ("Vishal");
+		System.out.println("String with new keyword:"+ c);
+		
+		
+	}
+
+}
+
+
+
+//
+//String q = "Vishal S Chavan";      //String Literal
+//System.out.println(q);
+//
+////length of string
+//  int d1 = q.length();
+//System.out.println("length of string: "+q.length());
